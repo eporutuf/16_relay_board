b86582d1c6210aad84bb892bb851125e1291bc5e
michael <m.pey@futurope.com>
2020-02-25 11:45:40 +0100

    new IP + status + temp + current

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,11 @@
 #include "math.h"
 
 #define UDP_SOCKET 12345
-char IP_Addr[30] = "192.168.11.";
-const char *IP_Subnet = "255.255.255.0";
-const char *IP_Gateway = "192.168.11.1";
+char IP_Addr[30] = "172.16.10.";
+const char *IP_Subnet = "255.255.0.0";
+const char *IP_Gateway = "172.16.0.1";
 
 uint8_t mac[] = {0x00, 0x08, 0xDC, 0x1C, 0xAA, 0xCA};
-
-
 
 SPI spi(PA_7, PA_6, PA_5);
 WIZnetInterface ethernet(&spi, PA_1, PA_0);
@@ -25,22 +23,24 @@ Endpoint recv_addr;
 
 void f_ethernet_init();
 void set_ip();
-void action_relay(char *payload);
-void ack_relay(char *payload);
+uint8_t action(char *payload);
+void ack_relay(char *payload, uint8_t info_action);
 
 int ret = -1;
 int ret_set = -1;
 int r_result = 0;
+uint8_t ret_action;
+
 char buff[256];
 
 int main()
 {
-    
+
     pc.baud(115200);
     addr.mode(PullNone);
     coil = (0);
     set_ip();
-    
+
     f_ethernet_init();
     while (ret == -1)
     {
@@ -52,7 +52,6 @@ int main()
         ret_set = recv_addr.set_address(IP_Gateway, UDP_SOCKET);
         wait(3);
     }
-    
 
     while (1)
     {
@@ -60,9 +59,8 @@ int main()
         if (r_result > 0)
         {
             pc.printf("[%d]recv[%s]:[%d] : %s \n", r_result, recv_addr.get_address(), recv_addr.get_port(), buff);
-
-            action_relay(buff);
-            ack_relay(buff);
+            ret_action = action(buff);
+            ack_relay(buff, ret_action);
         }
     }
     wait(1);
@@ -70,11 +68,11 @@ int main()
 
 void f_ethernet_init()
 {
-    
+
     // mbed_mac_address((char *)mac);
     pc.printf("\t Initialize Ethernet Service...\n\r");
     ethernet.reset();
-    
+
     wait(3);
     int ret = ethernet.init(mac, IP_Addr, IP_Subnet, IP_Gateway);
     if (!ret)
@@ -113,14 +111,14 @@ void set_ip()
     int value = addr.read();
     if (value == 0 || value == 1)
         value = 2;
-    sprintf(IP_Addr, "192.168.11.%d", value);
+    sprintf(IP_Addr, "172.16.10.%d", value);
     mac[5] = value;
 }
 
-void action_relay(char *payload)
+uint8_t action(char *payload)
 {
     //pc.printf("Payload is : %s \n", payload);
-    //scanf("%s",payload);
+    // scanf("%s",payload);
     int l = strlen(payload) - 1;
     //pc.printf("Length == %d \n", l);
     int relay = (payload[2] - 48) * 10 + (payload[3] - 48);
@@ -128,23 +126,64 @@ void action_relay(char *payload)
     //pc.printf("Numero Relay === %d \n", relay);
     //pc.printf("Etat Relay = %d", etat);
     //pc.printf("_____________________________ \r\n");
-    if(etat == 0)
+    if (relay != 0 && relay != 220 && relay != 190)
     {
-        coil = (0);
+        if (etat == 0)
+        {
+            coil = (0);
+        }
+        else if (etat == 1)
+        {
+            //pc.printf("Coil Value = %d",(pow(2,(relay -1))) );
+            coil = (pow(2, (relay - 1)));
+        }
+        return 0;
     }
-    else if (etat == 1)
+    else if (relay == 0 || relay == 220 || relay == 190)
     {
-        //pc.printf("Coil Value = %d",(pow(2,(relay -1))) );
-        coil = (pow(2,(relay -1)));
+        if (payload[2] == 48) //Status
+        {
+            return 1;
+        }
+        else if (payload[2] == 70) //Temp
+        {
+            return 2;
+        }
+        else if (payload[2] == 67) //Curent
+        {
+            return 3;
+        }
     }
 }
 
-void ack_relay(char *payload)
+// 0 -> relay ON/OFF ; 1 -> Status ; 2 -> Temp ; 3 -> Current
+void ack_relay(char *payload, uint8_t info_action)
 {
-
-    char ack[100] = "1"; 
-    strcat(ack,payload);
-    int l = strlen(ack) - 1;
-    pc.printf("Payload is : %s \n", ack);
-    client.sendTo(recv_addr,ack,l);
+    if (info_action == 0)
+    {
+        char ack[50] = "1;";
+        strcat(ack, payload);
+        int l = strlen(ack);
+        client.sendTo(recv_addr, ack, l);
+    }
+    else if (info_action == 1)
+    {
+        int statusRelay = coil.read();
+        char ack[50];
+        sprintf(ack, "%d", statusRelay);
+        int l = strlen(ack);
+        client.sendTo(recv_addr, ack, l);
+    }
+    else if (info_action == 2)
+    {
+        char ack[50] = "8.92";
+        int l = strlen(ack);
+        client.sendTo(recv_addr, ack, l);
+    }
+    else if (info_action == 3)
+    {
+        char ack[50] = "0.32";
+        int l = strlen(ack);
+        client.sendTo(recv_addr, ack, l);
+    }
 }
